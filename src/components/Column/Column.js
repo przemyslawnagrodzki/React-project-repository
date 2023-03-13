8e797c29e91459f7fb82856ee6d63fbb7d328ab9
Przemysław Nagrodzki <nagrodzkiprzemyslaw@gmail.com>
2023-03-13 12:31:24 +0100

    .

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -9,7 +9,7 @@ const Column = (props) => {
         <article className={styles.column}>
             <span className={styles.icon + ' fa fa-' + props.icon} />
             <h2 className={styles.title}>{props.title}</h2>
-            < ul className={styles.cards} >
+            < ul className={styles.titles} >
                 {props.cards.map(card => <li key={card.id}>{card.title}</li>)}
             </ul >
             <CardForm columnId={props.id} action={props.addCard} />
