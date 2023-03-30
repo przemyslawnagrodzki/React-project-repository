a7399181998836fd474223f59822de0142501b97
Przemysław Nagrodzki <nagrodzkiprzemyslaw@gmail.com>
2023-03-30 13:33:13 +0200

    Add Home

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import SearchForm from './components/SearchForm/SearchForm'
 import List from './components/List/List'
 import Container from './components/Container/Container'
 import NavBar from './components/NavBar/NavBar'
+import Home from './components/Home/Home'
 
 const App = () => {
   return (
     <main>
       <NavBar />
     <Container>
-      <Hero />
-      <SearchForm />
-      <List />
+    <Home></Home>
     </Container>
     </main>
   );
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from '../Hero/Hero'
+import SearchForm from '../SearchForm/SearchForm'
+import List from '../List/List'
+
+const Home = () => {
+    return (
+        <>  <Hero></Hero>
+            <SearchForm></SearchForm>
+            <List></List> </>
+    )
+}
+
+export default Home
